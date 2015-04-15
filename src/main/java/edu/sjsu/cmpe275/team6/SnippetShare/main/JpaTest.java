3b9e4a8ef6f921e83d609c06d77ac25b86c9e88d
Ryan Ye <yulinye.yy@gmail.com>
2015-04-14 17:39:15 -0700

    init user DAO

--- src/main/java/edu/sjsu/cmpe275/team6/SnippetShare/main/JpaTest.java
+++ src/main/java/edu/sjsu/cmpe275/team6/SnippetShare/main/JpaTest.java
@@ -1,7 +1,19 @@
 package edu.sjsu.cmpe275.team6.SnippetShare.main;
 
+import edu.sjsu.cmpe275.team6.SnippetShare.dao.UserDAO;
+import edu.sjsu.cmpe275.team6.SnippetShare.model.User;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by Corn on 4/6/15.
  */
 public class JpaTest {
+    public static void main(String[] args) {
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-module.xml");
+//
+//
+//        UserDAO userDAO = (UserDAO) context.getBean("userDAO");
+//
+//        userDAO.insert(new User("yulin", "yulin", "yulin@gmail.com", "xxx"));
+    }
 }
