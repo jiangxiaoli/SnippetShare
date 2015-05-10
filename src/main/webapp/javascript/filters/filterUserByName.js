4275c896c5fdc2f640ad64f412b987380eb7022b
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2015-05-10 16:03:22 -0700

    Search function: search boards, users, snippets.

---
+++ src/main/webapp/javascript/filters/filterUserByName.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module("snippetShare")
+    .filter('filterUserByName', function () {
+        return function (users, username) {
+
+            //if no board
+            if(!users || !users.length || !username)
+                return users;
+
+
+            console.log(username);
+
+            var filtered = [];
+            for(var i = 0; i< users.length; i++){
+                if(users[i].username.indexOf(username.toLowerCase()) != -1){
+                    filtered.push(users[i]);
+                }
+            }
+            return filtered;
+        }
+    });
